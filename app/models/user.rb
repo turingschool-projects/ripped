87abed2290ca501c76e86d929dd374352c7005b2
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-02-01 14:12:16 -0700

    Add user validations and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  validates :census_id, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is invalid without a census_id" do
+      user = User.create()
+      expect(user).to_not be_valid
+    end
+    it "is valid with all attributes" do
+      user = User.create(census_id: 1)
+      expect(user).to be_valid
+    end
+  end
 end
