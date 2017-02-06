c10646daf11f1710465ac923604cee72ca6fd94e
Anna Dolan <anna.dolan@gmail.com>
2017-02-05 17:51:10 -0700

    Add links to solution show pages, update tests

--- spec/features/dashboard/user_sees_exercises_sorted_by_status_on_dashboard_spec.rb
+++ spec/features/dashboard/user_sees_exercises_sorted_by_status_on_dashboard_spec.rb
@@ -5,14 +5,14 @@ describe "/users/:id" do
     user = User.create(census_id: 1)
     exercise1 = create(:exercise)
     exercise2 = create(:exercise)
-    solution1 = create(:solution, exercise_id: exercise1.id, user_id: user.id, status: "Submitted")
-    solution2 = create(:solution, exercise_id: exercise2.id, user_id: user.id, status: "Solved")
+    solution1 = create(:solution, exercise_id: exercise2.id, user_id: user.id, status: "Submitted")
+    solution2 = create(:solution, exercise_id: exercise1.id, user_id: user.id, status: "Solved")
     visit user_path(user)
 
     expect(page).to have_content("Exercises in Progress")
 
     within(:css, "#in-progress") do
-      expect(page).to have_content(solution1.exercise.name)
+      expect(page).to have_link(solution1.exercise.name, href: exercise_solution_path(exercise2, solution1))
       expect(page).to_not have_content(solution2.exercise.name)
     end
   end
@@ -21,13 +21,13 @@ describe "/users/:id" do
     user = User.create(census_id: 1)
     exercise1 = create(:exercise)
     exercise2 = create(:exercise)
-    solution1 = create(:solution, exercise_id: exercise1.id, user_id: user.id, status: "Submitted")
-    solution2 = create(:solution, exercise_id: exercise2.id, user_id: user.id, status: "Solved")
+    solution1 = create(:solution, exercise_id: exercise2.id, user_id: user.id, status: "Submitted")
+    solution2 = create(:solution, exercise_id: exercise1.id, user_id: user.id, status: "Solved")
     visit user_path(user)
 
     expect(page).to have_content("Completed Exercises")
     within(:css, "#completed") do
-      expect(page).to have_content(solution2.exercise.name)
+      expect(page).to have_link(solution2.exercise.name, href: exercise_solution_path(exercise1, solution2))
       expect(page).to_not have_content(solution1.exercise.name)
     end
   end
