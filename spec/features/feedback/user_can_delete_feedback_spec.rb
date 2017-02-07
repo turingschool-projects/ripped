6d75a7f9b9c3a80106bb3812631a7334bf168258
Anna Dolan <anna.dolan@gmail.com>
2017-02-07 11:42:24 -0700

    User does not see a delete button for feedback left by another user

--- spec/features/feedback/user_can_delete_feedback_spec.rb
+++ spec/features/feedback/user_can_delete_feedback_spec.rb
@@ -20,4 +20,21 @@ describe "a user visits a show page for another user's solution that they have l
     expect(page).to_not have_content("Comment1")
     expect(page).to have_content("Your feedback has been deleted.")
   end
+
+  scenario "and does not see a delete button next to feedback from other users" do
+    user = create(:user)
+    user2 = create(:user)
+    user3 = create(:user)
+    exercise = create(:exercise)
+    solution = create(:solution, user: user2, exercise: exercise)
+    feedback = create(:feedback, solution_id: solution.id, user: user, comment: "Comment1")
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user3)
+
+    visit exercise_solution_path(exercise, solution)
+
+    expect(page).to have_content("Comment1")
+
+    expect(page).to_not have_content("Delete")
+  end
 end
