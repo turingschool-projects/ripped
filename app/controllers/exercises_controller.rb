231aa5506254b214276bba61ed9d16c0253b22f5
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-02-07 14:25:15 -0700

    added links to partials

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,6 +2,8 @@ class ExercisesController < ApplicationController
 
   def index
     @exercises = Exercise.all
+    @js_exercises = Exercise.joins(:tags).where("tag_id = 1")
+    @ruby_exercises = Exercise.joins(:tags).where("tag_id = 2")
   end
 
   def show
--- spec/features/exercises/user_can_see_exercises_spec.rb
+++ spec/features/exercises/user_can_see_exercises_spec.rb
@@ -9,4 +9,20 @@ describe "/exercises" do
     expect(page).to have_content(exercise.name)
     expect(page).to have_content(exercise.description)
   end
+
+  it "a user can view exercises by track" do
+    tag_1 = create(:tag, name: "js")
+    tag_2 = create(:tag, name: "ruby")
+    exercise_js = create(:exercise)
+    exercise_js_1 = create(:exercise)
+    exercise_ruby = create(:exercise)
+    exercise_js.tags = [tag_1]
+    exercise_js_1.tags = [tag_1]
+    exercise_ruby.tags = [tag_2]
+
+    visit exercises_path
+
+    expect(page).to have_content(exercise_js.name)
+    expect(page).to have_content(exercise_js.description)
+  end
 end
