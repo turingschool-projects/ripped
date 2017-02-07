28b5b236784868585e88bc6ec93f5e521425689c
Anna Dolan <anna.dolan@gmail.com>
2017-02-06 20:22:02 -0700

    Add migration and model tests for feedback

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,16 @@
-ActiveRecord::Schema.define(version: 20170205172503) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170207031354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -16,6 +28,16 @@ ActiveRecord::Schema.define(version: 20170205172503) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.text     "description"
+  end
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "solution_id"
+    t.text     "comment"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["solution_id"], name: "index_feedbacks_on_solution_id", using: :btree
+    t.index ["user_id"], name: "index_feedbacks_on_user_id", using: :btree
   end
 
   create_table "solutions", force: :cascade do |t|
@@ -43,6 +65,8 @@ ActiveRecord::Schema.define(version: 20170205172503) do
 
   add_foreign_key "exercise_tags", "exercises"
   add_foreign_key "exercise_tags", "tags"
+  add_foreign_key "feedbacks", "solutions"
+  add_foreign_key "feedbacks", "users"
   add_foreign_key "solutions", "exercises"
   add_foreign_key "solutions", "users"
 end
