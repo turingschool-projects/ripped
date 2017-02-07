16a21a9bc9c6ea9b2de5c27dcdeb95f7d28c78e3
Noah Berman <noah@noahberman.org>
2017-02-07 11:44:42 -0700

    notification logo shows up only when logged in

--- spec/features/user/user_sees_notifications_spec.rb
+++ spec/features/user/user_sees_notifications_spec.rb
@@ -1,18 +1,27 @@
 require "rails_helper"
 
-describe "after a user logs in" do
-  it "sees a list of exercises" do
-    stub = stub_omniauth
-    user = User.create!(census_id: stub.uid)
-    exercise = create(:exercise)
-    visit '/'
-    expect(page).to have_content('Login with Census')
+describe "when a user visits the site" do
+  context "and they are not logged in" do
+    scenario "they do not see a notification bell" do
+      visit '/'
+      
+      expect(page).to have_content('Login with Census')
+      expect(page).to_not have_css("#notification-logo")
+    end
+  end
+  
+  context "and they are logged in" do
+    scenario "they do see a notification bell" do
+      stub = stub_omniauth
+      user = User.create!(census_id: stub.uid)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit '/'
+      expect(page).to have_content('Login with Census')
 
-    click_link "Login with Census"
-    expect(page).to have_content("Exercises")
-    expect(page).to have_content(exercise.name)
-    expect(page).to have_content(exercise.description)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      click_link "Login with Census"
+      expect(page).to have_css("#notification-logo")
+    end
   end
 end
