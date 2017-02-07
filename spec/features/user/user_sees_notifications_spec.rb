7f0d0059bfe89f1dd6901ea8386ee8e8c6dc2601
Noah Berman <noah@noahberman.org>
2017-02-07 15:37:21 -0700

    notification button is displaying the correct number

--- spec/features/user/user_sees_notifications_spec.rb
+++ spec/features/user/user_sees_notifications_spec.rb
@@ -23,5 +23,54 @@ describe "when a user visits the site" do
       click_link "Login with Census"
       expect(page).to have_css("#notification-logo")
     end
+    
+    scenario "they see a number next to the notification alert" do
+      stub = stub_omniauth
+      exercise = create(:exercise)
+      user_1 = User.create!(census_id: stub.uid)
+      solution_1 = Solution.create!(content: "Hello", user_id: user_1.id, exercise_id: exercise.id, status: 0)
+
+      visit '/'
+      expect(page).to have_content('Login with Census')
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+      click_link "Login with Census"
+      expect(page).to have_content("0")
+    end
+    
+    scenario "they see the correct number next to the notification alert" do
+      stub = stub_omniauth
+      exercise = create(:exercise)
+      user_1 = User.create!(census_id: stub.uid)
+      solution_1 = Solution.create!(content: "Hello", user_id: user_1.id, exercise_id: exercise.id, status: 1)
+
+      visit '/'
+      expect(page).to have_content('Login with Census')
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+      click_link "Login with Census"
+      expect(page).to have_content("1")
+    end
+
+    scenario "they see the correct number next to the notification alert" do
+      stub_1 = stub_omniauth
+      stub_2 = stub_omniauth
+      exercise_1 = create(:exercise)
+      exercise_2 = create(:exercise)
+      user_1 = User.create!(census_id: stub_1.uid, role: 1)
+      user_2 = User.create!(census_id: stub_2.uid, role: 0)
+      solution_1 = Solution.create!(content: "Hello", user_id: user_2.id, exercise_id: exercise_1.id, status: 0)
+      solution_2 = Solution.create!(content: "Hello", user_id: user_2.id, exercise_id: exercise_2.id, status: 0)
+
+      visit '/'
+      expect(page).to have_content('Login with Census')
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+      click_link "Login with Census"
+      expect(page).to have_content("2")
+    end
   end
 end
