504dea4dd088d02bcc2016d21fce0a5361ef21a7
Anna Dolan <anna.dolan@gmail.com>
2017-02-06 22:08:38 -0700

    Add sad path test for feedback

--- spec/features/feedback/user_leaves_feedback_on_a_solution_spec.rb
+++ spec/features/feedback/user_leaves_feedback_on_a_solution_spec.rb
@@ -21,17 +21,21 @@ describe "a user isits a show page for another user's solution" do
 
   end
 
-  # scenario "and sees a form to add feedback" do
-  #   user = create(:user)
-  #   user2 = create(:user)
-  #   exercise = create(:exercise)
-  #   solution = create(:solution, user: user2, exercise: exercise)
-  #
-  #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-  #
-  #   visit exercise_solution_path(exercise, solution)
-  #
-  #   expect(page).to have_content("Leave Feedback")
-  #   expect(page).to have_xpath('//textarea[@id="feedback-content"]')
-  # end
+  scenario "and does not add any content in the comment form" do
+    user = create(:user)
+    user2 = create(:user)
+    exercise = create(:exercise)
+    solution = create(:solution, user: user2, exercise: exercise)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit exercise_solution_path(exercise, solution)
+
+    fill_in "feedback-content", with: ""
+    click_button 'Create Feedback'
+
+    expect(current_path).to eq(exercise_solution_path(exercise, solution))
+    expect(page).to have_content("Feedback")
+    expect(page).to have_content("Something went wrong.")
+  end
 end
