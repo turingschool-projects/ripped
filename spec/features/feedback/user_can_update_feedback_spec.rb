01c88ebdc3b2da613410861fc9d1121b4103fba9
Anna Dolan <anna.dolan@gmail.com>
2017-02-07 11:15:15 -0700

    User can update feedback

--- spec/features/feedback/user_can_update_feedback_spec.rb
+++ spec/features/feedback/user_can_update_feedback_spec.rb
@@ -19,5 +19,30 @@ describe "a user visits a show page for another user's solution that they have l
     expect(current_path).to eq(edit_solution_feedback_path(solution, feedback))
     expect(page).to have_content("Edit your feedback")
     expect(page).to have_xpath('//textarea[@id="feedback-update"]')
+    expect(page).to have_button("Update Feedback")
+  end
+
+  scenario "and edits their feedback" do
+    user = create(:user)
+    user2 = create(:user)
+    exercise = create(:exercise)
+    solution = create(:solution, user: user2, exercise: exercise)
+    feedback = create(:feedback, solution_id: solution.id, user: user, comment: "Comment1")
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit exercise_solution_path(exercise, solution)
+
+    expect(page).to have_content("Comment1")
+
+    click_link "Edit Feedback"
+
+    fill_in "feedback-update", with: "Sup dawg"
+
+    click_button "Update Feedback"
+
+    expect(current_path).to eq(exercise_solution_path(exercise, solution))
+    expect(page).to have_content("Sup dawg")
+    expect(page).to_not have_content("Comment1")
   end
 end
