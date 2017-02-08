de7fdc1dc73a5d5e973e97f5799a8008323ee4fc
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-02-08 11:17:53 -0700

    added instructor test to exercises

--- spec/features/exercises/user_can_see_exercises_spec.rb
+++ spec/features/exercises/user_can_see_exercises_spec.rb
@@ -1,6 +1,17 @@
 require "rails_helper"
 
 describe "/exercises" do
+  it "instructor can view exercises" do
+    exercise = create(:exercise)
+    user = create(:user, role: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit exercises_path
+
+    expect(page).to have_content(exercise.name)
+    expect(page).to have_content(exercise.description)
+  end
+  
   it "a user can view exercises by track" do
     tag_1 = create(:tag, name: "javascript")
     tag_2 = create(:tag, name: "ruby")
