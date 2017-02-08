3f6f2d4be946d15d9d89484438db51206c60ae4b
Anna Dolan <anna.dolan@gmail.com>
2017-02-08 16:01:53 -0700

    API setup complete

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   authorize_resource class: false
-  
+
   def show
     @user = current_user
     @in_progress = @user.solutions.where(status: "Submitted")
