ae1f6d4111ab56a1dde097054b8da236d47084e0
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-02-07 21:13:31 -0700

    Set up permission rules with CanCanCan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,9 +5,16 @@ class Ability
     user || User.new
     if user.student?
       can [:index, :show], Exercise
+      can :show, :dashboard
+      can [:new, :create, :edit, :update, :destroy], Feedback, user_id: user.id
+      can [:new, :create], Solution
+      can [:index, :show], Solution, exercise: {solutions: {user_id: user.id}}
     end
     if user.instructor?
-      can [:index, :show, :new, :create, :edit, :update], Exercise
+      can [:index, :show, :new, :create, :edit, :update, :destroy], Exercise
+      can :show, :dashboard
+      can [:new, :create, :edit, :update, :destroy], Feedback
+      can [:index, :show, :new, :create], Solution
     end
   end
 end
