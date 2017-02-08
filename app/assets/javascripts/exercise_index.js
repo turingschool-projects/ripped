e5df89c3e8a87be138eae028c23bb4f7377412ed
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-02-08 12:26:55 -0700

    toggle added to exercise tracks

--- app/assets/javascripts/exercise_index.js
+++ app/assets/javascripts/exercise_index.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
 
+
+
 $('.ruby-button').click(function(){
   $("#js_exercises").hide()
-  $("#exercises #ruby_exercises").removeAttr("style");
+  $("#exercises #ruby_exercises").toggle();
 });
 
 $('.js-button').click(function(){
   $("#ruby_exercises").hide();
-  $("#exercises #js_exercises").removeAttr("style");
+  $("#exercises #js_exercises").toggle();
 });
 
 
