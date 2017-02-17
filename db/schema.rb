e81217bd044a10acd857c2e9e4a646ebc9e39c31
Noah Berman <noah@noahberman.org>
2017-02-16 21:25:58 -0700

    test and migration for feedback status enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208212706) do
+ActiveRecord::Schema.define(version: 20170217042441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,8 +35,9 @@ ActiveRecord::Schema.define(version: 20170208212706) do
     t.integer  "user_id"
     t.integer  "solution_id"
     t.text     "comment"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "status",      default: 0
     t.index ["solution_id"], name: "index_feedbacks_on_solution_id", using: :btree
     t.index ["user_id"], name: "index_feedbacks_on_user_id", using: :btree
   end
