5f6cc73722de00f60a77ee1dea8edaaddedd9598
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-02-19 18:22:28 -0700

    work on filter feature

--- app/assets/javascripts/exercise_index.js
+++ app/assets/javascripts/exercise_index.js
@@ -2,11 +2,20 @@ $(document).ready(function(){
 
   $('.tag_buttons').click(function(e){
     e.preventDefault();
-    // alert(this.name);
-    var tag = this.name;
+    $(this).toggleClass('active');
+    var activeTags = $.map($('.tag_buttons.active'), function(element){
+      return element.name;
+    });
+
     $('#all-exercises div').each(function(){
       var tags = $(this).attr('data-exercise').split(',');
-      if(tags.includes(tag)) {
+      var match = false;
+      activeTags.forEach(function(tag){
+        if(tags.includes(tag)){
+          match = true;
+        }
+      });
+      if(match) {
         $(this).show();
       } else {
         $(this).hide();
