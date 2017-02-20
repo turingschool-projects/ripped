72a64f9c441aac80544287812ca16719a6f959d0
Noah Berman <noah@noahberman.org>
2017-02-19 20:17:22 -0700

    first attempt at parsing down incoming feedbacks

--- app/assets/javascripts/components/_content.js.jsx
+++ app/assets/javascripts/components/_content.js.jsx
@@ -1,10 +1,7 @@
 var Content = React.createClass({
-  getInitialState() {
-    return { solutions: [] }
-  },
-  
   componentDidMount() {
-    $.getJSON('/api/v1/users/2/solutions', (response) => { this.setState({ solutions: response }) })
+    $.getJSON('/api/v1/users/2/solutions', (response) => { this.setState({ response }) })
+    debugger
   },
   
   handleDelete() {
@@ -12,19 +9,21 @@ var Content = React.createClass({
   },
   
   render(){
-    var feedbacks = this.state.solutions.map((solution) => {
-      return (
-        <div key={solution.id}>
-          <p>Comment on your solution for:</p>
-          <p>{solution.exercise.name}</p>
-          <button onClick={this.handleDelete}>X</button>
-        </div>
-      )
-    });
+    // var feedbacks = this.state.solutions.map((solution) => {
+    //   return (
+    //     <div key={solution.id}>
+    //       <li>
+    //         <p>Comment on your solution for:</p>
+    //         <p>{solution.exercise.name}</p>
+    //         <button onClick={this.handleDelete}>X</button>
+    //       </li>
+    //     </div>
+    //   )
+    // });
     
     return(
       <div>
-        { feedbacks }
+        Hey
       </div>
     )
   }
--- app/controllers/api/v1/solutions_controller.rb
+++ app/controllers/api/v1/solutions_controller.rb
@@ -9,11 +9,21 @@ class Api::V1::SolutionsController < ApiController
   end
   
   def index
-    @solutions = Solution.where(user_id: params[:user_id])
-    if !@solutions.nil?
-      render json: @solutions, status: 200
+    solutions = Solution.where(user_id: params[:user_id])
+    @unreads = find_only_unread_feedbacks(solutions)
+    if !@unreads.nil?
+      render json: @unreads, status: 200
     else
       render body: nil, status: 400
     end
+  end
+  
+  private
+  
+  def find_only_unread_feedbacks(solutions)
+    solutions_with_unread_feedbacks = solutions.map do |solution|
+      solution.feedbacks.where(status: "unread")
+    end
+    solutions_with_unread_feedbacks
   end
 end
