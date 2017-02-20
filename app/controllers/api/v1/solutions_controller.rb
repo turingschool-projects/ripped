47901d7c4be80f4c7ccf93e4ceb938d132f3319c
Noah Berman <noah@noahberman.org>
2017-02-19 20:40:56 -0700

    proper solution return in controller

--- app/controllers/api/v1/solutions_controller.rb
+++ app/controllers/api/v1/solutions_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::SolutionsController < ApiController
   
   def index
     solutions = Solution.where(user_id: params[:user_id])
-    @unreads = find_only_unread_feedbacks(solutions)
+    @unreads = find_solutions_for_unread_feedbacks(solutions)
     if !@unreads.nil?
       render json: @unreads, status: 200
     else
@@ -21,9 +21,24 @@ class Api::V1::SolutionsController < ApiController
   private
   
   def find_only_unread_feedbacks(solutions)
-    solutions_with_unread_feedbacks = solutions.map do |solution|
-      solution.feedbacks.where(status: "unread")
+    solutions_with_unread_feedbacks = []
+    solutions.each do |solution|
+      if !solution.feedbacks.where(status: "unread")[0].nil?
+        solutions_with_unread_feedbacks << solution.feedbacks.where(status: "unread")[0].solution_id
+      end
+      solutions_with_unread_feedbacks.uniq
     end
     solutions_with_unread_feedbacks
+  end
+  
+  def find_solutions_for_unread_feedbacks(solutions)
+    solutions_to_display = find_only_unread_feedbacks(solutions)
+    if !solutions_to_display.nil?
+      notifications = solutions_to_display.each do |solution|
+        Solution.where(id: solution)
+      end
+    solutions
+    end
+    solutions
   end
 end
