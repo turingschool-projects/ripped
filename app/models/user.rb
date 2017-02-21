80c455ec3fbec4092a2fa06e8fb7128413511972
Noah Berman <noah@noahberman.org>
2017-02-20 17:38:48 -0700

    feedback count displaying properly for student

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ApplicationRecord
       notifier_count = find_uncommented_solution_count
       display_info = instructor_notifier_count_handler(notifier_count)
     else
-      notifier_count = find_solutions_with_new_comments_count
+      notifier_count = find_solutions_with_new_comments_count(current_user)
       display_info = student_notifier_count_handler(notifier_count)
     end
     display_info
@@ -42,17 +42,17 @@ class User < ApplicationRecord
     elsif notifier_count == 1
       "1 Solution Needs Feedback"
     else
-      notifier_count.to_s + "Solutions Need Feedback"
+      notifier_count.to_s + " Solutions Need Feedback"
     end
   end
 
   def student_notifier_count_handler(notifier_count)
     if notifier_count == 0
-      "Recent Updates: None"
+      "No Unread Feedback"
     elsif notifier_count == 1
-      "1 Solution Has Feedback"
+      "1 Unread Feedback"
     else
-      notifier_count.to_s + "Solutions Have Feedback"
+      notifier_count.to_s + " Unread Feedbacks"
     end
   end
 
@@ -77,15 +77,23 @@ class User < ApplicationRecord
     count = uncommented_solutions.count
   end
   
-  def find_solutions_with_new_comments_count
-    solutions = Solutions.where(user_id: current_user.id)
-    commented_solutions = []
+  def find_solutions_with_new_comments_count(current_user)
+    solutions = Solution.where(user_id: current_user.id)
+    commented_solutions = 0
     solutions.each do |solution|
-      if solution.feedbacks.status = "unread"
-        commented_solutions = []
-      end
-      commented_solutions
+      commented_solutions += filter_feedbacks_for_solution(solution)
     end
-    count = commented_solutions.count
+    commented_solutions
+  end
+  
+  def filter_feedbacks_for_solution(solution)
+    feedbacks_that_need_to_be_read = []
+    solution.feedbacks.each do |feedback|
+      if feedback.status = "unread"
+        feedbacks_that_need_to_be_read << feedback
+      end
+      feedbacks_that_need_to_be_read
+    end
+    feedbacks_that_need_to_be_read.count
   end
 end
