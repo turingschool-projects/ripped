963fde3f3ec7bf0f369abccb7c13908d37ea29de
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-02-22 16:30:15 -0700

    git API calls changed

--- spec/services/census_service_spec.rb
+++ spec/services/census_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "CensusService" do
   context "#get_users" do
-    it "returns an array of users as JSON objects" do
+    it "returns an array of users as JSON objects", :vcr do
         users = CensusService.new(ENV["census_token"]).get_users
         user = users.first
 
@@ -14,7 +14,7 @@ describe "CensusService" do
     end
   end
   context "#get_one_user" do
-    it "returns a single user" do
+    it "returns a single user", :vcr do
         user = CensusService.new(ENV["census_token"]).get_one_user(55)
         expect(user).to be_a(Hash)
         expect(user[:first_name]).to eq("Anna")
