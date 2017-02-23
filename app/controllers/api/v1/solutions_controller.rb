ecdd19d92cd7fcee8eb84d617ecad890b69789db
Noah Berman <noah@noahberman.org>
2017-02-22 18:07:01 -0700

    remove unnecessary method from solution controller

--- app/controllers/api/v1/solutions_controller.rb
+++ app/controllers/api/v1/solutions_controller.rb
@@ -35,17 +35,4 @@ class Api::V1::SolutionsController < ApiController
   
   private
   
-  def find_only_unread_feedbacks(solutions)
-    solutions_with_unread_feedbacks = []
-    solutions.each do |solution|
-      if !solution.feedbacks.where(status: "unread")[0].nil?
-        solutions_with_unread_feedbacks << solution.feedbacks.where(status: "unread")[0].solution_id
-      end
-      solutions_with_unread_feedbacks.uniq
-    end
-    return_solutions = solutions_with_unread_feedbacks.map do |solution|
-      Solution.find(solution)
-    end
-    return_solutions
-  end
 end
